a691d60dc66b282f35033767eda79aae537f520f
JuLight69 <cheeju69@gmail.com>
2020-10-01 21:37:35 +0300

    hwork1 1-4ex

---
+++ src/main/java/ru/mail/polis/ads/part5.java
@@ -0,0 +1,30 @@
+package ru.mail.polis.ads;
+
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class part5 {
+    public static void main(String[] arg) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        boolean f = false;
+        int m = 0;
+        for(int i=0; i<line.length();i++){
+            if(line.charAt(i) == '('){
+                m++;
+            }else if(line.charAt(i) == ')'){
+                m--;
+            }
+            if(m<0){
+                f = true;
+            }
+        }
+        if(f || (m>0)){
+            System.out.println("NO");
+        }else{
+            System.out.println("YES");
+        }
+    }
+}
